cce7a3f330089e4ee94a205062224c1d17000032
harsh12436 <168220008+harsh12436@users.noreply.github.com>
2024-04-29 18:47:43 +0530

    getting price from wazirx

---
+++ price_waz.py
@@ -0,0 +1,22 @@
+import requests
+
+# API  for fetching prices
+url_waz = 'https://api.wazirx.com/api/v2/tickers'
+
+# Coin symbols
+coins_waz = ['btcinr', 'ethinr', 'filinr']
+
+# Fetching prices
+response = requests.get(url_waz)
+data_waz = response.json()
+
+
+price_waz= {}
+for element in data_waz:
+#element will give name of coin 
+    if element in coins_waz:
+        coin_symbol = element
+        last_price = data_waz[element]["last"]
+        price_waz[coin_symbol] = last_price
+        
+print(price_waz)
