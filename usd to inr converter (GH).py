8d778e00519d4c96fb9862c8c377456926a062d7
sesrk <168438294+sesrk@users.noreply.github.com>
2024-04-29 21:12:06 +0530

    Update usd to inr converter (GH).py

--- usd to inr converter (GH).py
+++ usd to inr converter (GH).py
@@ -1,21 +1,21 @@
 import requests
 def convert_usd_to_inr(amount):
         # api link for dollar to rupee converter
-        url = f'https://open.er-api.com/v6/latest/USD'
+        url=f'https://open.er-api.com/v6/latest/USD'
         try:
             # sending get request for usd to inr converter
-            response = requests.get(url)
-            data = response.json()
+            response=requests.get(url)
+            data=response.json()
             # if response is 200 it means api is fine and server is working properly
-            if response.status_code == 200:
+            if response.status_code==200:
                 #extracting the data ( exchange value for usd into inr)
-                exchange_rate = data['rates']['INR']
-                converted_amount = amount * exchange_rate
-                return round(converted_amount, 2)
+                exchange_rate=data['rates']['INR']
+                converted_amount=amount*exchange_rate
+                return round(converted_amount,2)
             else:
                 # printing that response errorcode.if its not 200(an error is there)
-                print(f"Unable to convert dollar to INR, error code: {response.status_code}")
+                print(f"unable to convert dollar to INR,error code:{response.status_code}")
                 return None
         except Exception as e:
-            print(f"An error occurred: {str(e)}")
+            print(f"an error occurred:{str(e)}")
             return None
